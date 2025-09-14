e34da1248ac1319c0791487ba9d6f42c1176027f
Pavel Palancica <pavel.palancica@gmail.com>
2025-09-14 14:04:26 +0300

    Added a CardView component

--- Memorize/ContentView.swift
+++ Memorize/ContentView.swift
@@ -2,13 +2,35 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        HStack() {
+            CardView(content: "👻", isFaceUp: false)
+            CardView(content: "👻", isFaceUp: true)
+            CardView(content: "👻", isFaceUp: true)
+            CardView(content: "👻", isFaceUp: true)
         }
         .padding()
+    }
+}
+
+struct CardView: View {
+    let content: String
+    @State var isFaceUp = false
+    
+    var body: some View {
+        ZStack {
+            let base = RoundedRectangle(cornerRadius: 12)
+            
+            if isFaceUp {
+                base.fill(.white)
+                base.strokeBorder(lineWidth: 2)
+                Text(content).font(.largeTitle)
+            } else {
+                base.fill()
+            }
+        }
+        .onTapGesture {
+            isFaceUp.toggle()
+        }
     }
 }
 
