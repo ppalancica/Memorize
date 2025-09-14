489bee926ae18cc028d3cdce8ef141ba000cec47
Pavel Palancica <pavel.palancica@gmail.com>
2025-09-14 22:34:15 +0300

    Added Int? return type to fix the choose method inside MemoryGame

--- Memorize/MemoryGame.swift
+++ Memorize/MemoryGame.swift
@@ -16,17 +16,18 @@ struct MemoryGame<CardContent> where CardContent: Equatable {
     
     mutating func choose(_ card: Card) {
         // card.isFaceUp.toggle() won't work, cause card is a copy and a let
-        let chosenIndex = index(of: card)
-        cards[chosenIndex].isFaceUp.toggle()
+        if let chosenIndex = index(of: card) {
+            cards[chosenIndex].isFaceUp.toggle()
+        }
     }
     
-    private func index(of card: Card) -> Int {
+    private func index(of card: Card) -> Int? {
         for index in cards.indices {
             if cards[index].id == card.id {
                 return index
             }
         }
-        return 0 // FIXME: bogus!
+        return nil
     }
     
     mutating func shuffle() {
