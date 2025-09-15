88646325395388ea6bc61c61044bb9921a8d1892
Pavel Palancica <pavel.palancica@gmail.com>
2025-09-15 14:30:48 +0300

    Removed the ScrollView that Card views where embedded in. Use the GeometryReader to calculate the proper sizes so they would always fit on the screen.

--- Memorize/EmojiMemoryGameView.swift
+++ Memorize/EmojiMemoryGameView.swift
@@ -6,29 +6,65 @@ struct EmojiMemoryGameView: View {
     
     var body: some View {
         VStack {
-            ScrollView {
-                cards
-                    .animation(.default, value: viewModel.cards)
-            }
+            // ScrollView {
+            cards
+                .animation(.default, value: viewModel.cards)
+                // .background(.red)
+            // }
             Button("Shuffle") {
                 viewModel.shuffle()
             }
+            // .background(Color.blue)
         }
+        // .background(.yellow)
         .padding()
+        // .background(.yellow) - will have different effect
     }
     
     var cards: some View {
-        LazyVGrid(columns: [GridItem(.adaptive(minimum: 85), spacing: 0)], spacing: 0) {
-            ForEach(viewModel.cards) { card in
-                CardView(card)
-                    .aspectRatio(2 / 3, contentMode: .fit)
-                    .padding(4)
-                    .onTapGesture {
-                        viewModel.choose(card)
-                    }
+        GeometryReader { geometry in
+            let gridItemSize = gridItemWidthThatFits(
+                count: viewModel.cards.count,
+                size: geometry.size,
+                atAspectRatio: 2 / 3
+            )
+            // We used to directly provide constants such as 65, 85, 95 for minimum, but not anymore
+            LazyVGrid(columns: [GridItem(.adaptive(minimum: gridItemSize), spacing: 0)], spacing: 0) {
+                ForEach(viewModel.cards) { card in
+                    CardView(card)
+                        .aspectRatio(2 / 3, contentMode: .fit)
+                        .padding(4)
+                        .onTapGesture {
+                            viewModel.choose(card)
+                        }
+                }
             }
         }
         .foregroundStyle(.orange)
+    }
+    
+    private func gridItemWidthThatFits(
+        count: Int,
+        size: CGSize,
+        atAspectRatio aspectRatio: CGFloat
+    ) -> CGFloat {
+        let count = CGFloat(count)
+        var columnCount = 1.0
+        
+        repeat {
+            let width = size.width / columnCount
+            let height = width / aspectRatio
+            
+            let rowCount = (count / columnCount).rounded(.up)
+            
+            if rowCount * height < size.height {
+                return (size.width / columnCount).rounded(.down)
+            }
+            
+            columnCount += 1
+        } while columnCount < count
+        
+        return min(size.width / count, size.height * aspectRatio).rounded(.down)
     }
 }
 
