4c6d8f2e5610bee282081001c119300acbed0dce
Pavel Palancica <pavel.palancica@gmail.com>
2025-09-16 12:23:12 +0300

    Keep the old CardView's body for reference

--- Memorize/CardView.swift
+++ Memorize/CardView.swift
@@ -36,6 +36,33 @@ struct CardView: View {
         .opacity(card.isFaceUp || !card.isMatched ? 1 : 0)
     }
     
+    // UNUSED
+    /* var body: some View {
+        ZStack {
+            let base = RoundedRectangle(cornerRadius: Constants.cornerRadius)
+            
+            Group {
+                base.fill(.white)
+                base.strokeBorder(lineWidth: Constants.lineWidth)
+                Pie(endAngle: .degrees(240))
+                    .opacity(Constants.Pie.opacity)
+                    .overlay(
+                        Text(card.content)
+                            .font(.system(size: Constants.FontSize.largest))
+                            .minimumScaleFactor(Constants.FontSize.scaleFactor) // Was 0.01
+                            .multilineTextAlignment(.center)
+                            .aspectRatio(1, contentMode: .fit)
+                            .padding(5)
+                    )
+                    .padding(Constants.Pie.inset)
+            }
+            .opacity(card.isFaceUp ? 1 : 0)
+            
+            base.fill().opacity(card.isFaceUp ? 0 : 1)
+        }
+        .opacity(card.isFaceUp || !card.isMatched ? 1 : 0)
+    } */
+    
     private struct Constants {
         static let cornerRadius: CGFloat = 12
         static let lineWidth: CGFloat = 2
