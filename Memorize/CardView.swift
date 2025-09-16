0ba138dde8c0dfed810c2656b274d98b037e5750
Pavel Palancica <pavel.palancica@gmail.com>
2025-09-16 10:09:55 +0300

    Moved CardView into its own file

---
+++ Memorize/CardView.swift
@@ -0,0 +1,34 @@
+import SwiftUI
+
+struct CardView: View {
+    let card: MemoryGame<String>.Card
+    
+    init(_ card: MemoryGame<String>.Card) {
+        self.card = card
+    }
+    
+    var body: some View {
+        ZStack {
+            let base = RoundedRectangle(cornerRadius: 12)
+            
+            Group {
+                base.fill(.white)
+                base.strokeBorder(lineWidth: 2)
+                Text(card.content)
+                    .font(.system(size: 200))
+                    .minimumScaleFactor(0.01)
+                    .aspectRatio(1, contentMode: .fit)
+            }
+            .opacity(card.isFaceUp ? 1 : 0)
+            
+            base.fill().opacity(card.isFaceUp ? 0 : 1)
+        }
+        .opacity(card.isFaceUp || !card.isMatched ? 1 : 0)
+    }
+}
+
+#Preview {
+    CardView(MemoryGame<String>.Card(content: "X", id: "TestID"))
+        .padding()
+        .foregroundStyle(.green)
+}
