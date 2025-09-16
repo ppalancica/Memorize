81bf2a74a0c5563b91d783dc094ccc5fbf3be147
Pavel Palancica <pavel.palancica@gmail.com>
2025-09-16 10:50:09 +0300

    Minor code improvement

--- Memorize/CardView.swift
+++ Memorize/CardView.swift
@@ -10,6 +10,27 @@ struct CardView: View {
         self.card = card
     }
     
+    var body: some View {
+        ZStack {
+            let base = RoundedRectangle(cornerRadius: Constants.cornerRadius)
+            
+            Group {
+                base.fill(.white)
+                base.strokeBorder(lineWidth: Constants.lineWidth)
+                Text(card.content)
+                    .font(.system(size: Constants.FontSize.largest))
+                    .minimumScaleFactor(Constants.FontSize.scaleFactor) // Was 0.01
+                    .multilineTextAlignment(.center)
+                    .aspectRatio(1, contentMode: .fit)
+                    .padding(Constants.inset)
+            }
+            .opacity(card.isFaceUp ? 1 : 0)
+            
+            base.fill().opacity(card.isFaceUp ? 0 : 1)
+        }
+        .opacity(card.isFaceUp || !card.isMatched ? 1 : 0)
+    }
+    
     private struct Constants {
         static let cornerRadius: CGFloat = 12
         static let lineWidth: CGFloat = 2
@@ -20,27 +41,6 @@ struct CardView: View {
             static let smallest: CGFloat = 10
             static let scaleFactor: CGFloat = smallest / largest
         }
-    }
-    
-    var body: some View {
-        ZStack {
-            let base = RoundedRectangle(cornerRadius: Constants.cornerRadius)
-            
-            Group {
-                base.fill(.white)
-                base.strokeBorder(lineWidth: Constants.lineWidth)
-                Text(card.content)
-                    .font(.system(size: Constants.FontSize.largest))
-                    .minimumScaleFactor(Constants.FontSize.smallest / Constants.FontSize.largest) // Was 0.01
-                    .multilineTextAlignment(.center)
-                    .aspectRatio(1, contentMode: .fit)
-                    .padding(Constants.inset)
-            }
-            .opacity(card.isFaceUp ? 1 : 0)
-            
-            base.fill().opacity(card.isFaceUp ? 0 : 1)
-        }
-        .opacity(card.isFaceUp || !card.isMatched ? 1 : 0)
     }
 }
 
