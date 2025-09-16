d2deda4521b7a83f18cced5345450b2d15c51478
Pavel Palancica <pavel.palancica@gmail.com>
2025-09-16 10:34:53 +0300

    Minor UI adjustments

--- Memorize/CardView.swift
+++ Memorize/CardView.swift
@@ -20,7 +20,9 @@ struct CardView: View {
                 Text(card.content)
                     .font(.system(size: 200))
                     .minimumScaleFactor(0.01)
+                    .multilineTextAlignment(.center)
                     .aspectRatio(1, contentMode: .fit)
+                    .padding(5)
             }
             .opacity(card.isFaceUp ? 1 : 0)
             
@@ -33,13 +35,20 @@ struct CardView: View {
 #Preview {
     
     // NOTE: cannot use typealias inside #Preview
-    // 
+    //
     // typealias Card = MemoryGame<String>.Card
     //
     
-    HStack {
-        CardView(CardView.Card(isFaceUp: true, content: "X", id: "TestID1"))
-        CardView(CardView.Card(content: "X", id: "TestID1"))
+    VStack {
+        HStack {
+            CardView(CardView.Card(isFaceUp: true, content: "X", id: "TestID1"))
+                .aspectRatio(4 / 3, contentMode: .fit)
+            CardView(CardView.Card(content: "X", id: "TestID1"))
+        }
+        HStack {
+            CardView(CardView.Card(isFaceUp: true, isMatched: true, content: "This is a very long text and I hope it fits", id: "TestID1"))
+            CardView(CardView.Card( isMatched: true, content: "X", id: "TestID1"))
+        }
     }
     .padding()
     .foregroundStyle(.green)
